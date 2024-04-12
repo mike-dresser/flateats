4187b60e2cf2bf15e1ef679f68fcf3001804a438
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-12 00:09:09 -0400

    Split CSS out by component, remove index.css

--- client/src/RestaurantCard.jsx
+++ client/src/RestaurantCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './RestaurantCard.css';
 
 function RestaurantCard({ restaurant }) {
   return (
-    <div>
+    <div className="card">
       <h3>{restaurant.name} </h3>
       <h4>{'$'.repeat(restaurant.price)} </h4>
       <p>{restaurant.cuisine}</p>
