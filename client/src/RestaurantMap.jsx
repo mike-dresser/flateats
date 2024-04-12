7f7f25118beb83c72cff181e173c38a13b35844d
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-11 22:29:06 -0400

    Fix map centering

--- client/src/RestaurantMap.jsx
+++ client/src/RestaurantMap.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 
 function RestaurantMap() {
-  const position = { lat: 40.7052878, lng: -74.0164789 };
+  const position = { lat: 40.7053707, lng: -74.0142418 };
   return (
     <APIProvider apiKey={import.meta.env.VITE_API_KEY}>
       <div id="mapContainer">
-        <Map center={position} zoom={15}>
+        <Map defaultCenter={position} zoom={16}>
           <Marker position={position} />
         </Map>
       </div>
