f424daf9ef829ff2b782274d7a31999ea464a7f0
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-12 13:23:37 -0400

    Migrate Marker -> AdvancedMarker

--- client/src/RestaurantMap.jsx
+++ client/src/RestaurantMap.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 // Imports for Google Maps API
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
 
 function RestaurantMap() {
   const position = { lat: 40.7053707, lng: -74.0142418 };
   return (
     <APIProvider apiKey={import.meta.env.VITE_API_KEY}>
       <div id="mapContainer">
-        <Map defaultCenter={position} defaultZoom={16}>
-          <Marker position={position} />
+        <Map defaultCenter={position} defaultZoom={16} mapId="9dff5b157a44a0e1">
+          <AdvancedMarker position={position} title="Flatiron School" />
         </Map>
       </div>
     </APIProvider>
