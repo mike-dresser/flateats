02083da817041f1d9cf84b444b05f26ce401c376
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-12 15:05:44 -0400

    Style homepage map pin

--- client/src/RestaurantMap.jsx
+++ client/src/RestaurantMap.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 // Imports for Google Maps API
-import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from '@vis.gl/react-google-maps';
 
 function RestaurantMap() {
   const position = { lat: 40.7053707, lng: -74.0142418 };
@@ -8,7 +13,14 @@ function RestaurantMap() {
     <APIProvider apiKey={import.meta.env.VITE_API_KEY}>
       <div id="mapContainer">
         <Map defaultCenter={position} defaultZoom={16} mapId="9dff5b157a44a0e1">
-          <AdvancedMarker position={position} title="Flatiron School" />
+          <AdvancedMarker position={position} title="Flatiron School">
+            <Pin
+              background="lightblue"
+              borderColor="black"
+              glyph="//"
+              scale={1.4}
+            />
+          </AdvancedMarker>
         </Map>
       </div>
     </APIProvider>
