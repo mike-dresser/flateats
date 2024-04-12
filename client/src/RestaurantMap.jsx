d1a271340a28e8102dcf7dc46f19c8b1e59dc321
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-12 19:20:47 -0400

    Render all pins on home map

--- client/src/RestaurantMap.jsx
+++ client/src/RestaurantMap.jsx
@@ -6,14 +6,20 @@ import {
   AdvancedMarker,
   Pin,
 } from '@vis.gl/react-google-maps';
+import { useLoaderData } from 'react-router-dom';
 
 function RestaurantMap() {
-  const position = { lat: 40.7053707, lng: -74.0142418 };
+  let allRestaurants = useLoaderData();
+  const flatiron_position = { lat: 40.7053707, lng: -74.0142418 };
   return (
     <APIProvider apiKey={import.meta.env.VITE_API_KEY}>
       <div id="mapContainer">
-        <Map defaultCenter={position} defaultZoom={16} mapId="9dff5b157a44a0e1">
-          <AdvancedMarker position={position} title="Flatiron School">
+        <Map
+          defaultCenter={flatiron_position}
+          defaultZoom={16}
+          mapId="9dff5b157a44a0e1"
+        >
+          <AdvancedMarker position={flatiron_position} title="Flatiron School">
             <Pin
               background="lightblue"
               borderColor="black"
@@ -21,6 +27,12 @@ function RestaurantMap() {
               scale={1.4}
             />
           </AdvancedMarker>
+          {allRestaurants.map((r) => (
+            <AdvancedMarker
+              position={{ lat: r.pos_lat, lng: r.pos_lon }}
+              title={r.name}
+            />
+          ))}
         </Map>
       </div>
     </APIProvider>
