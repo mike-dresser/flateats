81c9efb6a2e908eb8b3b28286caa6cd9405d7575
Samuel Chu <samueljchu0126@gmail.com>
2024-04-14 22:36:43 -0400

    fetch routing for restaurantpage

--- client/src/loaders.js
+++ client/src/loaders.js
@@ -2,9 +2,16 @@ async function restaurantListLoader({ request, params }) {
     const res = await fetch("http://127.0.0.1:5555/restaurants")
       .then(resp => resp.json())
     return res
-  }
+}
+
+async function restaurantLoader({ params }) {
+    const response = await fetch(`http://127.0.0.1:5555/restaurants/${params.id}`);
+    const data = await response.json();
+    return data;
+}
 
 export {
-    restaurantListLoader
+    restaurantListLoader,
+    restaurantLoader
 }
 
