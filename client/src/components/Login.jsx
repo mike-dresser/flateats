a3b504dbb0a06ead82863ee43c7ed04b6c04daa2
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-15 18:34:52 -0400

    Login almost working

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,8 +6,8 @@ function Login() {
 
   function handleLogin(e) {
     e.preventDefault();
-    console.log(e);
     let loginSubmission = { username: username, password: password };
+    console.log(loginSubmission);
 
     fetch(`http://127.0.0.1:5555/login`, {
       method: 'POST',
@@ -23,10 +23,11 @@ function Login() {
     <form onSubmit={(e) => handleLogin(e)}>
       <label htmlFor="username">Username</label>
       <input type="text" onChange={(e) => setUserName(e.target.value)}></input>
-      <label htmlFor="password" onChange={(e) => setPassword(e.target.value)}>
-        Password
-      </label>
-      <input type="password"></input>
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
       <input type="submit"></input>
     </form>
   );
