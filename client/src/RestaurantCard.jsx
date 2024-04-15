ee9b385a39931fd1fd4d1d6c2873e5da1c580b5f
Samuel Chu <samueljchu0126@gmail.com>
2024-04-15 11:44:25 -0400

    changes to restaurant card component with link routing

--- client/src/RestaurantCard.jsx
+++ client/src/RestaurantCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './RestaurantCard.css';
+import { Link } from "react-router-dom";
 
 function RestaurantCard({ restaurant }) {
   return (
     <div className="card">
-      <h3>{restaurant.name} </h3>
-      <h4>{'$'.repeat(restaurant.price)} </h4>
-      <p>{restaurant.cuisine}</p>
+    
+      <Link to ={`/restaurants/${restaurant.id}`}>
+        <h3>{restaurant.name} </h3>
+        <h4>{'$'.repeat(restaurant.price)} </h4>
+        <p>{restaurant.cuisine}</p>
+      </Link>
+      
     </div>
   );
 }
