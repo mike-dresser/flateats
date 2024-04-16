8708e11ee68d2863fce2015e49def777403f87ea
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-16 10:37:31 -0400

    Add home link to logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Header.css';
 import Login from '../Login';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div id="header">
       <div id="headerLeft">
         <h1>
-          <span>//</span>Flateats
+          <Link to="/">
+            <span>//</span>Flateats
+          </Link>
         </h1>
         <p>
           The <em>only</em> source for reliable lunch recommendations.
