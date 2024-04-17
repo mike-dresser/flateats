3f6d417de1e4263f7228b3524a51609dbad3592e
Samuel Chu <samueljchu0126@gmail.com>
2024-04-17 14:54:37 -0400

    add folders

---
+++ client/src/components/RestaurantList/RestaurantList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import RestaurantCard from '../RestaurantCard/RestaurantCard';
+import './RestaurantList.css'
+// import { useLoaderData } from 'react-router-dom';
+
+function RestaurantList({restaurants, search, setSearch}) {
+  const handleSearch = (e) => {
+    console.log(e.target.value)
+    setSearch(e.target.value)
+  }
+
+  let allRestaurants = restaurants;
+
+  
+  return (
+    <div>
+      <div className='searchBar'>
+        <input 
+          type='text'
+          className='search'
+          placeholder='Search Restaurants'
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="restaurant-list-container"> 
+        {restaurants.map((rest) => (
+          <RestaurantCard 
+            key={rest.id}
+            restaurant={rest}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RestaurantList;
